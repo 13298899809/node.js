6cefae10726b3290a557eb61071f3d4ceafb6be1
13298899809 <wujian1345528755>
2018-04-05 16:50:06 +0800

    文件系统

---
+++ 01_文件系统/04_同步文件写入_02.js
@@ -0,0 +1,14 @@
+/**
+ * fs.writeFileSync(file, data[, options])
+ * file <string> | <Buffer> | <integer> 文件名或文件描述符
+   data <string> | <Buffer> | <Uint8Array>
+   options <Object> | <string>
+   encoding <string> | <null> 默认 = 'utf8'
+   mode <integer> 默认 = 0o666
+   flag <string> 默认 = 'w
+ *
+ */
+var fs = require("fs");
+var file = "./hello3.txt";
+var data = "writeFileSync写入的内容";
+fs.writeFileSync(file,data);
---
+++ 01_文件系统/05_异步文件写入_02.js
@@ -0,0 +1,21 @@
+/**
+ * fs.writeFile(file, data[, options], callback)
+ * file <string> | <Buffer> | <integer> 文件名或文件描述符
+   data <string> | <Buffer> | <Uint8Array>
+   options <Object> | <string>
+   encoding <string> | <null> 默认 = 'utf8'
+   mode <integer> 默认 = 0o666
+   flag <string> 默认 = 'w'
+   callback <Function>
+   err <Error>
+ */
+var fs = require("fs");
+var file = "./hello4.txt";
+var data = "writeFile写入的内容";
+fs.writeFile(file,data,function(err) {
+if(!err){
+    console.log("成功");
+}else{
+    console.log("失败");
+}
+});

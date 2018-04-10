2981d4bbacc8937bb2c42da3597e424e5775e18c
13298899809 <wujian1345528755>
2018-04-10 21:48:05 +0800

    express

--- 05_express框架/01_路由/03_与模板引擎配合.js
+++ 05_express框架/01_路由/03_与模板引擎配合.js
@@ -5,7 +5,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("01_hello", {
+    res.render("../views/01_hello", {
         names: ["张三", "李四", "王五"]
     })
 });
